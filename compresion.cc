992a563795d29878e15eec01bf5ac54865dbd053
fedeboco <fedeboco@gmail.com>
2019-04-18 12:17:53 -0300

    Confirmo cambios de rulo

--- compresion.cc
+++ compresion.cc
@@ -1,19 +1,11 @@
 #include <iostream>
 #include "compresion.h"
 
-
 using namespace std;
 
 #define MSJ_ERROR_FOPEN "No se pudo abrir el archivo."
 #define MSJ_DIC_LLENO "Se lleno el diccionario, se procede a resetearlo"
 #define MSJ_ARCHIVO_VACIO "El archivo a tratar está vacío"
-
-/*bool cargarASCII(diccionario & dic)
-{
-    for(int i=0; i<=255; i++)
-		dic.asignar_secuencia(i,-1,char(i));
-    return 1;
-}*/
 
 bool comprimir(diccionario & dic, istream * iss, ostream *oss)
 {
@@ -21,35 +13,26 @@ bool comprimir(diccionario & dic, istream * iss, ostream *oss)
 	int buffer_size = 1000000;
 	diccionario buffer(buffer_size);
 
-	// para que no quede cargada al azar
 	dic.resetear_diccionario();
 
 	int P = -1;
 	int indice = -1;
 
-	//Para primeros 2 carácteres.
-	//El primero lo va a encontrar. El segundo no.
+	//Fede: Para primeros 2 carácteres. El primero lo va a encontrar. El segundo no.
 	if( (S = (*iss).get()) != EOF ){
-
-		 // busco primer caracter (encuentra)		
+	
 		indice = dic.buscar_secuencia(-1, S);
 		P = indice;
 		if( (S = (*iss).get()) != EOF ){
-			 //agrego primer secuencia (no encuentra)
 			dic.agregar_secuencia(P, S);
-			//imprimo salida
 			*oss << P; 
-			//busco la posicion de S sin prefijo
 			P = dic.buscar_secuencia(-1,S);
 		}
-		// RULO Si el próximo caracter está vacío lo imprime y sale de la función.
+		// Rulo: Si el próximo caracter está vacío lo imprime y sale de la función.
 		else
 		{
-			 //agrego primer secuencia (no encuentra)
 			dic.agregar_secuencia(P, S);
-			//imprimo salida
 			*oss << P; 
-			//busco la posicion de S sin prefijo
 			P = dic.buscar_secuencia(-1,S);
 			return 1;
 		}
@@ -61,51 +44,34 @@ bool comprimir(diccionario & dic, istream * iss, ostream *oss)
 		cout << MSJ_ARCHIVO_VACIO << endl;
 		return 1;
 	}
-		
 
-	
-
+	//Fede: Desde el tercer caracter hasta el final.	
 	int j = 0;
 	while( (S = (*iss).get()) != EOF )
 	{	
-		indice = dic.buscar_secuencia(P, S); // busco secuencia
-		
-		//Si no la encuentro
+		indice = dic.buscar_secuencia(P, S);
 		if( indice == -1 )
 		{
-			//la agrego al final
 			dic.agregar_secuencia(P, S);
-			//agrego a buffer para imprimir
 			buffer.asignar_secuencia(j, P, S);
-			//busco la posicion de S sin prefijo
 			indice = dic.buscar_secuencia(-1,S);
-
 			j++;
-
 		}
 		P = indice;
 
-		//vacío buffer
+		//Fede: Vacío buffer
 		if( j >= buffer_size )
-		{	
-			
-			//*oss << buffer.obtener_P(i); //imprimo salida (1ero)
+		{			
 			j = 0;
 			for(int i = 0; i < buffer_size; i++)
-			{
 				*oss << "," << buffer.obtener_P(i); //imprimo salida
-			}
 		}
 	}
 
+	//Fede: Vacío lo que quedó del buffer
 	buffer.asignar_secuencia(j, P, S);
-
-	//vacío lo que quedó del buffer
-	
 	for(int i = 0; i <= j; i++)
-	{
 		*oss << "," << buffer.obtener_P(i);
-	}
 	
     return 1;
 }
@@ -140,9 +106,7 @@ bool descomprimir(diccionario & dic, istream * iss, ostream *oss)
     	aux = int(indice_actual_aux)-48;
     	indice_actual = indice_actual * 10+ aux;
     }
-    //*oss << "Primer Indice: " << indice_actual << " Corresponde a:" << dic.obtener_S(indice_actual) << endl;
 	*oss << dic.obtener_S(indice_actual);
-	//cout << "Primer Indice: " << indice_actual << " Corresponde a:" << dic.obtener_S(indice_actual) << endl;
     while ((*iss).eof() == false)        
     {
         indice_anterior = indice_actual;
